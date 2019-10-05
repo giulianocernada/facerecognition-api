cb4944ed23aaa0bdc871c3601220cfd6b66f0fb7
Giuliano Cernada <giuliano.cernada@hotmail.com>
2019-10-05 00:09:27 -0300

    making it work

--- server.js
+++ server.js
@@ -14,10 +14,12 @@ const db = knex({
   }
 });
 
+// console.log(db.select('*').from('users'));
+ 
 const app = express();
-app.use(bodyParser.json()); 
-app.use(cors());
 
+app.use(cors());
+app.use(bodyParser.json()); 
 
 app.get('/', (req, res) => {
 	res.send(database.users);
@@ -27,18 +29,17 @@ app.post('/signin', (req, res) => {
 	db.select('email', 'hash').from('login')
 	.where('email', '=', req.body.email)
 	.then(data => {
-		const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-		if(isValid) {
+		const isValid =  bcrypt.compareSync(req.body.password, data[0].hash);
+		if(isValid){
 			return db.select('*').from('users')
-			.where('email', '=', req.body.email)
-			.then(user => {
-				res.json(user[0])
-			})
-			.catch(err => res.status(400).json('unable to get user'))
+				.where('email', '=', req.body.email)
+				.then(user => {
+					res.json(user[0])
+				})
+				.catch(err => res.status(400).json('Unable to get user'))
 		} else {
 			res.status(400).json('wrong credentials')
 		}
-
 	})
 	.catch(err => res.status(400).json('wrong credentials'))
 })
@@ -57,43 +58,42 @@ app.post('/register', (req, res) => {
 			return trx('users')
 			.returning('*')
 			.insert({
-			email: loginEmail[0],
-			name: name,
-			joined: new Date()
-		})
-		.then(user => {
-		res.json(user[0]);
-		})
+				email: loginEmail[0],
+				name: name,
+				joined: new Date()
+			})
+			.then(user => {
+			res.json(user[0]);
+			})
 		})
 		.then(trx.commit)
 		.catch(trx.rollback)
 	})
-	.catch(err => res.status(400).json('unable to register'))
+	.catch(err => res.status(400).json('Unable to register'))
 })
 
 app.get('/profile/:id', (req, res) =>{
 	const { id } = req.params;
 	db.select('*').from('users').where({id})
-	.then(user => {
-		if(user.length){
-			res.json(user[0])
-		} else {
-			res.status(400).json('not found')
-		}
-		
-	})
-	.catch(err => res.status(400).json('error getting user'))
+		.then(user => {
+			if(user.length){
+				res.json(user[0])
+			} else {
+				res.status(400).json('Not found!')
+			}		
+		})
+		.catch(err => res.status(400).json('Error gettin user!'))
 })
 
 app.put('/image', (req, res) => {
 	const { id } = req.body;
 	db('users').where('id', '=', id)
   	.increment('entries', 1)
-  	.returning('entries')
+  	.returning('entries') 
   	.then(entries => {
   		res.json(entries[0]);
   	})
-  	.catch(err => res.status(400).json('unable to get entries'))
+  	.catch(err => res.status(400).json('Unable to get entries'))
 })
 
 
