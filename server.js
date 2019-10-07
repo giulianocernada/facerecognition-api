dcfb7739b6fe232f6575fc49b1dcad25cc72756e
Giuliano Cernada <giuliano.cernada@hotmail.com>
2019-10-07 16:46:41 -0300

    changes

--- server.js
+++ server.js
@@ -12,8 +12,10 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    connectionString : process.env.DATABASE_URL,
-    ssl: true
+  		host: 'postgresql-dimensional-12007',
+  		user: 'giulianocernada',
+  		password: '',
+  		database: 'smart-brain'
   }
 });
 
